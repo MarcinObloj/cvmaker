57222df7c8f3f18a2f8f0dedd92a93f951fe45e4
Marcin <Marcix444@gmail.com>
2024-08-02 12:50:43 +0200

    added 2 sections, blog and 3steps

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,61 @@
 const burgerBtn = document.querySelector('.hamburger');
 const navMobile = document.querySelector('.nav__mobile');
 const navLi = document.querySelectorAll('.nav__mobile-item a');
+const stepOneBtn = document.querySelector('.threeSteps__btn-one');
+const stepTwoBtn = document.querySelector('.threeSteps__btn-two');
+const stepThreeBtn = document.querySelector('.threeSteps__btn-three');
+const bottomBoxOne = document.querySelector('.threeSteps-box-one');
+const bottomBoxTwo = document.querySelector('.threeSteps-box-two');
+const bottomBoxThree = document.querySelector('.threeSteps-box-three');
+
+const allStepsBtns = [stepOneBtn, stepTwoBtn, stepThreeBtn];
+const allBottomBoxes = [bottomBoxOne, bottomBoxTwo, bottomBoxThree];
+console.log(allBottomBoxes); console
+let currentIndex = 0;
+let intervalId;
+
+const handleBtnStep = (event) => {
+    allStepsBtns.forEach(btn => btn.classList.remove('active'));
+    allBottomBoxes.forEach(box => box.classList.remove('active-box'));
+
+    event.currentTarget.classList.add('active');
+    const index = allStepsBtns.indexOf(event.currentTarget);
+    allBottomBoxes[index].classList.add('active-box');
+
+    currentIndex = (index + 1) % allStepsBtns.length;
+
+    clearInterval(intervalId);
+    intervalId = setInterval(cycleActiveClass, 10000);
+};
+
 const handleBurger = () => {
-	navMobile.classList.toggle('nav__mobile--active');
-	burgerBtn.classList.toggle('is-active');
+    navMobile.classList.toggle('nav__mobile--active');
+    burgerBtn.classList.toggle('is-active');
 };
-console.log(navLi);
+
 const handleNavLinks = () => {
-	navMobile.classList.remove('nav__mobile--active');
-	burgerBtn.classList.remove('is-active');
+    navMobile.classList.remove('nav__mobile--active');
+    burgerBtn.classList.remove('is-active');
 };
+
 burgerBtn.addEventListener('click', handleBurger);
 navLi.forEach((link) => {
-	link.addEventListener('click', handleNavLinks);
+    link.addEventListener('click', handleNavLinks);
 });
+stepOneBtn.addEventListener('click', handleBtnStep);
+stepTwoBtn.addEventListener('click', handleBtnStep);
+stepThreeBtn.addEventListener('click', handleBtnStep);
+
+const cycleActiveClass = () => {
+    allStepsBtns.forEach(btn => btn.classList.remove('active'));
+    allBottomBoxes.forEach(box => box.classList.remove('active-box'));
+
+    allStepsBtns[currentIndex].classList.add('active');
+    allBottomBoxes[currentIndex].classList.add('active-box');
+
+    currentIndex = (currentIndex + 1) % allStepsBtns.length;
+};
+
+intervalId = setInterval(cycleActiveClass, 10000);
+
+cycleActiveClass();
