86a929993aceb3c82bd16b70c1f9de3b6b3311cd
Marcin <Marcix444@gmail.com>
2024-08-04 17:05:08 +0200

    created admin-panel site

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,11 @@ const stepThreeBtn = document.querySelector('.threeSteps__btn-three');
 const bottomBoxOne = document.querySelector('.threeSteps-box-one');
 const bottomBoxTwo = document.querySelector('.threeSteps-box-two');
 const bottomBoxThree = document.querySelector('.threeSteps-box-three');
-
+const allBtnsDashboard=document.querySelectorAll('.account-button')
 const allStepsBtns = [stepOneBtn, stepTwoBtn, stepThreeBtn];
 const allBottomBoxes = [bottomBoxOne, bottomBoxTwo, bottomBoxThree];
-console.log(allBottomBoxes); console
+console.log(allBtnsDashboard);
+
 let currentIndex = 0;
 let intervalId;
 
@@ -27,6 +28,10 @@ const handleBtnStep = (event) => {
     clearInterval(intervalId);
     intervalId = setInterval(cycleActiveClass, 10000);
 };
+const handleBtnActive = (e) => {
+    allBtnsDashboard.forEach(btn => btn.classList.remove('active'));
+    e.currentTarget.classList.add('active');
+};
 
 const handleBurger = () => {
     navMobile.classList.toggle('nav__mobile--active');
@@ -37,7 +42,9 @@ const handleNavLinks = () => {
     navMobile.classList.remove('nav__mobile--active');
     burgerBtn.classList.remove('is-active');
 };
-
+allBtnsDashboard.forEach(btn => {
+    btn.addEventListener('click', handleBtnActive);
+});
 burgerBtn.addEventListener('click', handleBurger);
 navLi.forEach((link) => {
     link.addEventListener('click', handleNavLinks);
@@ -55,6 +62,7 @@ const cycleActiveClass = () => {
 
     currentIndex = (currentIndex + 1) % allStepsBtns.length;
 };
+
 
 intervalId = setInterval(cycleActiveClass, 10000);
 
